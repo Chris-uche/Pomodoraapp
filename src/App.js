26f5e2e564a0653dba5935bdb357efc1002276a5
Chris Uche <38696564+Chris-uche@users.noreply.github.com>
2021-10-11 07:54:50 +0100

    added a js file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import React,{ useState} from "react";
 import './App.css';
 
+const padTimer=(timer)=>{
+  return timer.toString().padStart(2,"0")
+  
+}
+
+
 function App() {
-  const [timeLeft, setTimer]  = useState(25* 60)
-  const minutes  = Math.floor(timeLeft/60)
-  const seconds = timeLeft - minutes * 60
+  const [title, setTitle] = useState("Let the count down begin !")
+  const [timeLeft, setTimer]  = useState(10)
+  const minutes  = padTimer(Math.floor(timeLeft/60))
+  const seconds = padTimer(timeLeft - minutes * 60)
+
+  const startTimer=()=>{
+    setInterval(()=>{
+      setTimer( timeLeft=> {
+        if(timeLeft >=1){
+          return timeLeft -1
+          
+        }
+        return 0
+      }
+        
+      )
+      
+    },1000)
+  }
   
   return (
     <div className="app">
-      <h2>Pomodora!</h2>
+      <h2>{title}</h2>
       <div className="timer">
         <span>{minutes}</span>
         <span>:</span>
@@ -16,7 +38,7 @@ function App() {
         
       </div>
       <div className="button">
-        <button>Start</button>
+        <button onClick={startTimer}>Start</button>
         <button>Stop</button>
         <button>Reset</button>
         
